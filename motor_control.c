95258658b0c40b7405e7407a105da702e0d90611
kouhei ito <kouhei.ito@itolab-ktc.com>
2021-09-09 17:13:41 +0900

    new file

--- motor_control.c
+++ motor_control.c
@@ -14,9 +14,49 @@
 #define UART_TX_PIN 0
 #define UART_RX_PIN 1
 
-int main() {
-    /// \tag::setup_pwm[]
+static int chars_rxed = 0;
+static int data_num=0;
+uint8_t sbus_data[25];
+uint8_t ch;
 
+int main() {
+    /// シリアル通信の設定
+    stdio_init_all();
+    // Set up our UART with a basic baud rate.
+    uart_init(UART_ID, 2400);
+
+    // Set the TX and RX pins by using the function select on the GPIO
+    // Set datasheet for more information on function select
+    gpio_set_function(UART_TX_PIN, GPIO_FUNC_UART);
+    gpio_set_function(UART_RX_PIN, GPIO_FUNC_UART);
+
+    // Actually, we want a different speed
+    // The call will return the actual baud rate selected, which will be as close as
+    // possible to that requested
+    int actual = uart_set_baudrate(UART_ID, BAUD_RATE);
+
+    // Set UART flow control CTS/RTS, we don't want these, so turn them off
+    uart_set_hw_flow(UART_ID, false, false);
+
+    // Set our data format
+    uart_set_format(UART_ID, DATA_BITS, STOP_BITS, PARITY);
+
+    // Turn off FIFO's - we want to do this character by character
+    uart_set_fifo_enabled(UART_ID, false);
+
+    // Set up a RX interrupt
+    // We need to set up the handler first
+    // Select correct interrupt for the UART we are using
+    int UART_IRQ = UART_ID == uart0 ? UART0_IRQ : UART1_IRQ;
+
+    // And set up and enable the interrupt handlers
+    irq_set_exclusive_handler(UART_IRQ, on_uart_rx);
+    irq_set_enabled(UART_IRQ, true);
+
+    // Now enable the UART to send interrupts - RX only
+    uart_set_irq_enables(UART_ID, true, false);
+
+    // PWMの設定
     // Tell GPIO 0 and 1 they are allocated to the PWM
     gpio_set_function(2, GPIO_FUNC_PWM);
     gpio_set_function(3, GPIO_FUNC_PWM);
@@ -38,6 +78,7 @@ int main() {
     pwm_set_chan_level(slice_num, PWM_CHAN_A, 1330);
     sleep_ms(5000);
     while(true){
+      tight_loop_contents();
       pwm_set_chan_level(slice_num, PWM_CHAN_A, 1450);
       sleep_ms(2000);
       pwm_set_chan_level(slice_num, PWM_CHAN_A, 1330);
@@ -45,14 +86,6 @@ int main() {
     }
     
 }
-
-
-
-static int chars_rxed = 0;
-static int data_num=0;
-uint8_t sbus_data[25];
-
-uint8_t ch;
 
 // RX interrupt handler
 void on_uart_rx() {
@@ -112,69 +145,6 @@ void on_uart_rx() {
             chars_rxed=0;
         }
         
-    }
-}
-
-int main() {
-    stdio_init_all();
-    // Set up our UART with a basic baud rate.
-    uart_init(UART_ID, 2400);
-
-    // Set the TX and RX pins by using the function select on the GPIO
-    // Set datasheet for more information on function select
-    gpio_set_function(UART_TX_PIN, GPIO_FUNC_UART);
-    gpio_set_function(UART_RX_PIN, GPIO_FUNC_UART);
-
-    // Actually, we want a different speed
-    // The call will return the actual baud rate selected, which will be as close as
-    // possible to that requested
-    int actual = uart_set_baudrate(UART_ID, BAUD_RATE);
-
-    // Set UART flow control CTS/RTS, we don't want these, so turn them off
-    uart_set_hw_flow(UART_ID, false, false);
-
-    // Set our data format
-    uart_set_format(UART_ID, DATA_BITS, STOP_BITS, PARITY);
-
-    // Turn off FIFO's - we want to do this character by character
-    uart_set_fifo_enabled(UART_ID, false);
-
-    // Set up a RX interrupt
-    // We need to set up the handler first
-    // Select correct interrupt for the UART we are using
-    int UART_IRQ = UART_ID == uart0 ? UART0_IRQ : UART1_IRQ;
-
-    // And set up and enable the interrupt handlers
-    irq_set_exclusive_handler(UART_IRQ, on_uart_rx);
-    irq_set_enabled(UART_IRQ, true);
-
-    // Now enable the UART to send interrupts - RX only
-    uart_set_irq_enables(UART_ID, true, false);
-
-    // OK, all set up.
-    // Lets send a basic string out, and then run a loop and wait for RX interrupts
-    // The handler will count them, but also reflect the incoming data back with a slight change!
-    uart_puts(UART_ID, "\nHello, uart interrupts\n");
-    printf("Hello!\n");
-
-    while (1){
-        tight_loop_contents();
-        //printf("%x\n",ch);
-    }
-}
-
-/// \end:uart_advanced[]
-
-
-    while (1){
-        tight_loop_contents();
-        //printf("%x\n",ch);
-    }
-}
-
-    while (1){
-        tight_loop_contents();
-        //printf("%x\n",ch);
     }
 }
 
